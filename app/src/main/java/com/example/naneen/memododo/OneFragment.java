1378df6ba6d9ea19be547edafbb7b9611fa643b6
naneen <neen_tta@hotmail.com>
2017-05-23 14:05:39 +0700

    get input text on click submitBtn and show toast

--- app/src/main/java/com/example/naneen/memododo/OneFragment.java
+++ app/src/main/java/com/example/naneen/memododo/OneFragment.java
@@ -1,19 +1,20 @@
 package com.example.naneen.memododo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.example.naneen.memododo.R;
+import android.widget.Toast;
 
 public class OneFragment extends Fragment{
     Button addBtn;
-    EditText word, meanign;
+    EditText wordEdit, meaningEdit;
+//    String word, meaning;
 
     public OneFragment() {
         // Required empty public constructor
@@ -29,10 +30,34 @@ public class OneFragment extends Fragment{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_one, container, false);
-        addBtn = (Button) getView().findViewById(R.id.add);
-        word   = (EditText) getView().findViewById(R.id.word);
-        meanign   = (EditText) getView().findViewById(R.id.meaning);
+        addBtn = (Button) view.findViewById(R.id.addBtn);
+        wordEdit   = (EditText) view.findViewById(R.id.wordEdit);
+        meaningEdit   = (EditText) view.findViewById(R.id.meaningEdit);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String word = wordEdit.getText().toString().trim();
+                if (word.isEmpty()) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Please enter title", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+//                Firebase childRef_name = mRootRef.child("rd, Toast.LENGImage_Title");
+//                childRef_name.setValue(mName);
+                Toast.makeText(getActivity().getApplicationContext(), "Updated word: " + word, Toast.LENGTH_LONG).show();
+                InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        });
+
+//        addBtn.setOnClickListener((View.OnClickListener) this);
 
         return view;
     }
+//    @Override
+//    public void onClick(View v) {
+//        word = wordEdit.getText().toString();
+//    }
 }
